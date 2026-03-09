a42641c9f8d00ea7d1d86c0b132beeb4c661ab2a
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-09 11:11:15 +0530

    added decorators

---
+++ src/decorators/jwt.guard.ts
@@ -0,0 +1,54 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException
+} from '@nestjs/common';
+
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+
+    constructor(
+        private jwtService: JwtService,
+        private reflector: Reflector
+    ) { }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+
+        const isPublic = this.reflector.getAllAndOverride<boolean>(
+            IS_PUBLIC_KEY,
+            [
+                context.getHandler(),
+                context.getClass(),
+            ],
+        );
+
+        if (isPublic) return true;
+
+        const request = context.switchToHttp().getRequest();
+
+        const authHeader = request.headers.authorization;
+
+        if (!authHeader) {
+            throw new UnauthorizedException('Token missing');
+        }
+
+        const token = authHeader.split(' ')[1];
+
+        try {
+
+            const payload = await this.jwtService.verifyAsync(token);
+
+            request.user = payload;
+
+            return true;
+
+        } catch (err) {
+            throw new UnauthorizedException('Invalid token');
+        }
+    }
+}
