4974d30a0d64c7e437aeb963d9051f80b80eb26e
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-09 11:04:37 +0530

    configured app module connected mogodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Loads .env file
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+    AuthModule,
+    UserModule, 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule implements OnModuleInit {
+  constructor(@InjectConnection() private connection: Connection) { }
+  private readonly logger = new Logger('Database');
+  
+  onModuleInit() {
+    if (this.connection.readyState === 1) {
+      this.logger.log('MongoDB connected successfully');
+    } else {
+      this.logger.error('MongoDB connection failed');
+    }
+  }
+}
