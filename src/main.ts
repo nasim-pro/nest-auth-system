08529f9bff5ea3cf8833074cedb6c4be62d1e0aa
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-09 11:05:56 +0530

    added global pipe to validate dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,            // remove unknown fields
+      forbidNonWhitelisted: true, // throw error if extra fields
+      transform: true,            // auto transform DTO types
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
+  logger.log(`🚀 Server is running on: http://localhost:${process.env.PORT ?? 3000}`);
 }
 bootstrap();
